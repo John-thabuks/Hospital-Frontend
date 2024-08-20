d03006168cc6cf30c46343c62cf093a89a13ce52
John-thabuks <thabuksjohn@gmail.com>
2024-08-20 12:10:32 +0300

    feat(Welcome page)

--- src/Consent/Welcome.js
+++ src/Consent/Welcome.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import './welcome.css'
+import WelcomeImage from "../RelativeImages/WelcomePage.png"
+import welcomeCalender from "../RelativeImages/welcomeCalender.png"
 
 const Welcome = () => {
     return (
         <div>
             <table class="custom-table">
                 <tr>
-                    <td>Row 1, Column 1</td>
-                    <td>Row 1, Column 2</td>
-                    <td>Row 1, Column 3</td>
-                    <td>Row 1, Column 4</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>Row 2, Column 1</td>
-                    <td>Row 2, Column 2</td>
-                    <td>Row 2, Column 3</td>
-                    <td>Row 2, Column 4</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>Row 3, Column 1</td>
-                    <td>Row 3, Column 2</td>
-                    <td>Row 3, Column 3</td>
-                    <td>Row 3, Column 4</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
             </table>
+            <div className='firstHeading'>
+                <h2>Get Started</h2>
+                <p>Start yoour appointment journey and management</p>
+                <button className='gettingStarted'>Get Started </button>
+                <button>My Appointment</button>
+            </div>
+
+            <div>
+                <img src={WelcomeImage} alt="Welcome" className='Welcomeimg' />
+            </div>
+
+            <div className='quote'>
+                <p>"</p>
+                <p>Make it easy to keep track of your appointment</p>
+            </div>
+
+            <div className='welcomeCalender'>
+                <img src={welcomeCalender} alt="Calender" />
+            </div>
         </div>
     );
 }
